b0797eae6ccb897ca536d7026ed30dc84699c181
Diego Gutierrez da Silva <diego.gutierrezdasilva@gmail.com>
2025-05-02 11:31:08 -0300

    appsepec

--- infra/main.tf
+++ infra/main.tf
@@ -158,13 +158,13 @@ resource "aws_lb_target_group" "green" {
 }
 
 resource "aws_codedeploy_app" "ecs_app" {
-  name = "ecs-blue-green-app"
+  name = "AppECS-CalculadoraCluster-CalculadoraApiServices"
   compute_platform = "ECS"
 }
 
 resource "aws_codedeploy_deployment_group" "ecs_dg" {
   app_name               = aws_codedeploy_app.ecs_app.name
-  deployment_group_name  = "ecs-blue-green-dg"
+  deployment_group_name  = "DgpECS-CalculadoraCluster-CalculadoraApiServices"
   service_role_arn       = aws_iam_role.codedeploy_role.arn
   deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
 
